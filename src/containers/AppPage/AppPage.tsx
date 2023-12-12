14572431926447818add8a7f08e286ef537323eb
estebanix <dakisunshine@gmail.com>
2023-12-12 12:02:39 +0100

    simple map

--- src/containers/AppPage/AppPage.tsx
+++ src/containers/AppPage/AppPage.tsx
@@ -1,11 +1,14 @@
 import { HeaderMenu } from "../../components/HeaderMenu";
+import { MapPlot } from "../../components/MapPlot";
 
+import {data} from "./../../data/data";
 import styles from "./AppPage.module.scss";
 
 export const AppPage = () => {
     return (
         <div className={styles.appPageContainer}>
             <HeaderMenu />
+            <MapPlot data={data} width={1050} height={600} />
         </div>
     );
 }
---
+++ src/utils/constans/routes.ts
@@ -0,0 +1,7 @@
+export const HOME_PAGE_PATH = "/";
+export const APPS_PAGE_PATH = "/apps"
+
+export const ABOUT_PAGE_PATH = "/about";
+export const BLOG_PAGE_PATH = "/blog";
+export const DATAPROCESSING_PAGE_PATH = "/data_processing";
+export const VISUALIZATION_PAGE_PATH = "/visualization"
