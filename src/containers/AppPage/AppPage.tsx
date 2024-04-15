0b232089f581018950bdee28e7fbddb76d99af70
estebanix <dakisunshine@gmail.com>
2024-04-15 13:10:34 +0200

    Random plot trial

--- src/containers/AppPage/AppPage.tsx
+++ src/containers/AppPage/AppPage.tsx
@@ -1,14 +1,15 @@
+import { Density } from "../../components/BoxPlot/BoxPlot";
 import { SideMenu } from "../../components/SideMenu/SideMenu";
+
+import {data} from "./../../data/data";
 
 import styles from "./AppPage.module.scss";
 
 export const AppPage = () => {
-    return (
-        <div className={styles.appPageContainer}>
-            <SideMenu />
-            <div>
-                
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className={styles.appPageContainer}>
+      <SideMenu />
+      <Density width={800} height={500} data={data} />
+    </div>
+  );
+};
